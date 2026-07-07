40d03235cffd1e30b016fd6c1a87bd7bce249b0e
FotihYusupov <yusupovfotih158@gmail.com>
2026-07-07 17:24:06 +0500

    feat: initialize BotModule with dependencies for user, message, and log modules

--- src/modules/bot/bot.module.ts
+++ src/modules/bot/bot.module.ts
@@ -1,11 +1,11 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { BotService } from './bot.service';
 import { UsersModule } from '../users/users.module';
 import { MessagesModule } from '../messages/messages.module';
 import { LogsModule } from '../logs/logs.module';
 
 @Module({
-  imports: [UsersModule, MessagesModule, LogsModule],
+  imports: [forwardRef(() => UsersModule), MessagesModule, LogsModule],
   providers: [BotService],
   exports: [BotService]
 })
